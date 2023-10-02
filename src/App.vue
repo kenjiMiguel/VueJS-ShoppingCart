d3fade4e09d81336750068f799fceb37f5ce3980
kenjiMiguel <144721484+kenjiMiguel@users.noreply.github.com>
2023-10-02 18:54:35 +0000

    ficher :en lasado con checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <script setup>
-import { ref } from 'vue'; 
-const header = ref(' App Lista De Compras')
+import { ref } from 'vue';
+const header = ref('App Lista de compras');
 const items = ref([
-  { id: 1, label: '10 bolillos' },
-  { id: 2, label: '1 lata de frijoles' },
-  { id: 3, label: '2 lata de atún' },  
+   {id: 1, label: '10 bolillos' },
+   {id: 2, label: '1 lata de frijoles' },
+   {id: 3, label: '2 lata de atún' },
+]);
+const newItem = ref('');
+const newItemHighPriority = ref(false);
 
-  ]);
 </script>
 
 <template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header}} </h1> 
-  <ul>
-  <li v-for="({ id, label }, index) in items" v-bind:key="id">
-    🔹 - {{ index }} - {{ label }}
-  </li>
-</ul>
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+  <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo"> 
+  <!-- Radio buttons -->
+ <label>   
+ <input v-model="newItemHighPriority" 
+ type="checkbox">
+ Alta prioridad
+</label>
+{{ newItemPriority === "high" ?  "🔥" : "🧊"}}
 
-  </template> 
+  <ul>
+    <li v-for="{ id, label }  in items" v-bind:key="id">
+			🔹 {{ label }}
+		</li>
+  </ul>
+</template>
 
   <style scoped> 
   .shopping-cart-icon{
   font-size: 2rem;
   }
    </style>
+
 
