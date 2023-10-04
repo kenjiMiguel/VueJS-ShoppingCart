716b8ac3cd6263631d3c9285fe384bd52f7b6488
kenjiMiguel <144721484+kenjiMiguel@users.noreply.github.com>
2023-10-04 18:03:44 +0000

    ⭐Feature: Eventos de Usuario

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,40 @@
 import { ref } from 'vue';
 const header = ref('App Lista de compras');
 const items = ref([
-   {id: 1, label: '10 bolillos' },
-   {id: 2, label: '1 lata de frijoles' },
-   {id: 3, label: '2 lata de atún' },
+  {id: 1, label: '10 bolillos'},
+  {id: 2, label: '1 lata de frijoles'},
+  {id: 3, label: '2 lata de atún'}
 ]);
 const newItem = ref('');
 const newItemHighPriority = ref(false);
-
 </script>
 
 <template>
   <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo"> 
-  <!-- Radio buttons -->
- <label>   
- <input v-model="newItemHighPriority" 
- type="checkbox">
- Alta prioridad
-</label>
-{{ newItemPriority === "high" ?  "🔥" : "🧊"}}
-
+  <div class="add-item form">
+    <!-- Input de Nuevo Articulo -->
+    <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
+    <!-- Check Boxes -->
+    <label>
+      <input v-model="newItemHighPriority" 
+      type="checkbox">
+      Alta Prioridad
+    </label>
+    {{ newItemHighPriority ? "🔥" : "🧊" }}
+    <!-- Boton de UI -->
+    <button v-on:click="items.push({id: items.length + 1, label: newItem })" class="btn btn-primary">Salvar Articulo</button>
+  </div>
   <ul>
-    <li v-for="{ id, label }  in items" v-bind:key="id">
-			🔹 {{ label }}
-		</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">
+      🔹 {{ label }}
+    </li>
   </ul>
 </template>
 
-  <style scoped> 
-  .shopping-cart-icon{
-  font-size: 2rem;
-  }
-   </style>
+<style scoped>
+.shopping-cart-icon {
+  font-size: 2rem; /* Adjust the font-size value as per your desired size */
+}
+</style>
 
 
