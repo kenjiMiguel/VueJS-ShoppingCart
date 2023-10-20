b96a04218f9fee30f2dc247aa5d9d8215a9b9cac
kenjiMiguel <144721484+kenjiMiguel@users.noreply.github.com>
2023-10-20 04:55:43 +0000

    Feature : Cancelar boton

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <script setup>
 import { ref } from 'vue';
+
 const header = ref('App Lista de compras');
-const items = ref([
-  {id: 1, label: '10 bolillos'},
-  {id: 2, label: '1 lata de frijoles'},
-  {id: 3, label: '2 lata de atún'}
-]);
-// Agregado de metodo para guardar nuevo articulo en la lista
+const items = ref([]);
+
 const saveItem = () => {
-  items.value .push({id: items.value.length + 1, label: newItem.value})
-  // limpiando el contenido  de newItems
-  newItem.value ="";
+  items.value.push({ id: items.value.length + 1, label: newItem.value });
+  newItem.value = '';
 };
+
 const newItem = ref('');
 const newItemHighPriority = ref(false);
+const isNewItemVisible = ref(false); // Inicialmente, la caja de texto está oculta
+
+const showNewItemInput = () => {
+  isNewItemVisible.value = true;
+};
+
+const hideNewItemInput = () => {
+  isNewItemVisible.value = false;
+};
 </script>
 
 <template>
-  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-  <form v-on:submit.prevent="saveItem" class="add-item form">
+  <div class="header">
+    <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+    <button class="btn" @click="hideNewItemInput">Cancelar</button>
+    <button class="btn btn-primary" @click="showNewItemInput">Agregar articulos</button>
+  </div>
+  <form v-if="isNewItemVisible" v-on:submit.prevent="saveItem" class="add-item form">
     <!-- Input de Nuevo Articulo -->
     <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
     <!-- Check Boxes -->
     <label>
-      <input v-model="newItemHighPriority" 
-      type="checkbox">
+      <input v-model="newItemHighPriority" type="checkbox">
       Alta Prioridad
     </label>
     {{ newItemHighPriority ? "🔥" : "🧊" }}
@@ -36,7 +45,10 @@ const newItemHighPriority = ref(false);
       🔹 {{ label }}
     </li>
   </ul>
+  <p v-if="items.length === 0"> 🥀 lista de compras vacía 🥀 </p>
+  <p v-else>🔥 ingrese más items </p>
 </template>
+
 
 <style scoped>
 .shopping-cart-icon {
