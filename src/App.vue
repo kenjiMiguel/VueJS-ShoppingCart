8fffaebbe0941ec6124415e435049046fe5e16c0
kenjiMiguel <144721484+kenjiMiguel@users.noreply.github.com>
2023-10-23 18:27:16 +0000

    ⭐FEAT:App.vue     Agrega visualisacion condicionada al formulario

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <script setup>
 import { ref } from 'vue';
-
 const header = ref('App Lista de compras');
-const items = ref([]);
-
+const items = ref([
+ // {id: 1, label: '10 bolillos'},
+  //{id: 2, label: '1 lata de frijoles'},
+  //{id: 3, label: '2 lata de atún'}
+]);
+// Agregado de metodo para guardar nuevo articulo en la lista
 const saveItem = () => {
-  items.value.push({ id: items.value.length + 1, label: newItem.value });
-  newItem.value = '';
+  items.value .push({id: items.value.length + 1, label: newItem.value})
+  // limpiando el contenido  de newItems
+  newItem.value ="";
 };
-
 const newItem = ref('');
 const newItemHighPriority = ref(false);
-const isNewItemVisible = ref(false); // Inicialmente, la caja de texto está oculta
-
-const showNewItemInput = () => {
-  isNewItemVisible.value = true;
-};
-
-const hideNewItemInput = () => {
-  isNewItemVisible.value = false;
+const editing =ref(true);
+const doEdit = (edit) => {
+  // altero la variable "editing"
+  editing.value = edit;
+  // limpio el input de texto
+  newItem.value = ""; 
 };
 </script>
 
 <template>
   <div class="header">
+
     <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-    <button class="btn" @click="hideNewItemInput">Cancelar</button>
-    <button class="btn btn-primary" @click="showNewItemInput">Agregar articulos</button>
+    <button  v-if ="!editing" @click="doEdit (true)" class="btn bnt-primary">Agregar articulos</button>
+    <button v-else @click="doEdit (false)" class="btn">Cancelar</button>
   </div>
-  <form v-if="isNewItemVisible" v-on:submit.prevent="saveItem" class="add-item form">
+    
+  <form  v-if="editing" v-on:submit.prevent="saveItem" class="add-item form">
     <!-- Input de Nuevo Articulo -->
     <input v-model.trim="newItem" type="text" placeholder="Ingresar nuevo articulo">
     <!-- Check Boxes -->
     <label>
-      <input v-model="newItemHighPriority" type="checkbox">
+      <input v-model="newItemHighPriority" 
+      type="checkbox">
       Alta Prioridad
     </label>
     {{ newItemHighPriority ? "🔥" : "🧊" }}
@@ -45,10 +49,9 @@ const hideNewItemInput = () => {
       🔹 {{ label }}
     </li>
   </ul>
-  <p v-if="items.length === 0"> 🥀 lista de compras vacía 🥀 </p>
-  <p v-else>🔥 ingrese más items </p>
+  <p v-if="items.length === 0"> 🥀 lista de compras vacia 🥀 </p>
+  <p v-else>🔥 ingrese mas items </p>
 </template>
-
 
 <style scoped>
 .shopping-cart-icon {
